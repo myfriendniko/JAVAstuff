2bd6bb159e0b8c2d6d33f3435a4e6b9e00591c49
Victor <160630262+myfriendniko@users.noreply.github.com>
2025-08-28 10:52:19 -0300

    Update CalculadoraTeste.java

--- CalculadoraTeste.java
+++ CalculadoraTeste.java
@@ -20,30 +20,37 @@ public class CalculadoraTeste {
 		System.out.println(" Add 10+ 20 = 30");
 			int resposta = calc1.adicao();
 			System.out.println( "adicao " + resposta);
-		
+		mostrarMensagem(resposta == 30);
 		
 		System.out.println(" Subb 10 - 20 = 10");
 			resposta = calc1.subtracao();
 			System.out.println(resposta);
-		
+		mostrarMensagem(resposta == 10);
 		
 		System.out.println(" Multiplique 10 * 20 = 200");
 			double resposta1 = calc1.multiplicacao();
 			System.out.println(resposta1);
+		mostrarMensagem(resposta1 == 200);
 		
 		System.out.println(" Divida 10 / 20 = 0.5");
-			double resposta1 = calc1.divisao();
-			System.out.println(resposta1);
-		
-		
-		
-		
-		Calculadora calc2;
+			double resposta2 = calc1.divisao();
+			System.out.println(resposta2);
+		mostrarMensagem(resposta2 == 0.5);
+	}
+	
+	  static void mostrarMensagem(boolean teste){
+	        final String ANSI_GREEN = "\u001B[32m";
+	        final String ANSI_RED   = "\u001B[31m";
+	        final String ANSI_RESET = "\u001B[0m";
+
+	        System.out.println( (teste ? ANSI_GREEN : ANSI_RED) +
+	        		            (teste ? "Passou": "Não passou") +
+	        		             ANSI_RESET);
+	    }		
+	
 		
 	
 	
 	
 	}
 	
-	
-}
